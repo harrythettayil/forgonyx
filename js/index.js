fe2f78c23f2d60e9e71e3fcf0c04093950977301
Harry <t.a.harry@gmail.com>
2025-02-12 17:46:55 +0530

    animation added, shop commented

---
+++ js/index.js
@@ -0,0 +1,18 @@
+window.addEventListener("load", function () {
+  const fadeIns = document.querySelectorAll(".fade-in");
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("active");
+        }
+      });
+    },
+    { threshold: 0.5 }
+  );
+
+  fadeIns.forEach((fadeIn) => {
+    observer.observe(fadeIn);
+  });
+});
